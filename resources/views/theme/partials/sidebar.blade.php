eaeb2711e0880cf994da624d5030dbc508e6ae7f
sdad0o <slwdsaw@gmail.com>
2024-12-24 16:42:03 +0300

    create categories table and create seeder file and display the categories

--- resources/views/theme/partials/sidebar.blade.php
+++ resources/views/theme/partials/sidebar.blade.php
@@ -1,118 +1,102 @@
-          <!-- Start Blog Post Siddebar -->
-          <div class="col-lg-4 sidebar-widgets">
-              <div class="widget-wrap">
-                  <div class="single-sidebar-widget newsletter-widget">
-                      <h4 class="single-sidebar-widget__title">Newsletter</h4>
-                      @if (session('status'))
-                          <div class="alert alert-success">
-                              {{ session('status') }}
-                          </div>
-                      @endif
-                      <form action="{{ route('subscriber.store') }}"method="post">
-                          @csrf
-                          <div class="form-group mt-30">
-                              <div class="col-autos">
+     @php
+         use App\Models\Category;
+         $categories = Category::get();
+     @endphp
+     <!-- Start Blog Post Siddebar -->
+     <div class="col-lg-4 sidebar-widgets">
+         <div class="widget-wrap">
+             <div class="single-sidebar-widget newsletter-widget">
+                 <h4 class="single-sidebar-widget__title">Newsletter</h4>
+                 @if (session('status'))
+                     <div class="alert alert-success">
+                         {{ session('status') }}
+                     </div>
+                 @endif
+                 <form action="{{ route('subscriber.store') }}"method="post">
+                     @csrf
+                     <div class="form-group mt-30">
+                         <div class="col-autos">
 
-                                  <input type="text"name="email" class="form-control" id="inlineFormInputGroup"
-                                      placeholder="Enter email" onfocus="this.placeholder = ''"
-                                      onblur="this.placeholder = 'Enter email'" value="{{ old('email') }}">
-                                  @error('email')
-                                      <span class="text-danger">{{ $message }}</span>
-                                  @enderror
-                              </div>
-                          </div>
-                          <button type="submit" class="bbtns d-block mt-2 w-100">Subcribe</button>
-                      </form>
-                  </div>
+                             <input type="text"name="email" class="form-control" id="inlineFormInputGroup"
+                                 placeholder="Enter email" onfocus="this.placeholder = ''"
+                                 onblur="this.placeholder = 'Enter email'" value="{{ old('email') }}">
+                             @error('email')
+                                 <span class="text-danger">{{ $message }}</span>
+                             @enderror
+                         </div>
+                     </div>
+                     <button type="submit" class="bbtns d-block mt-2 w-100">Subcribe</button>
+                 </form>
+             </div>
+             @if (count($categories) > 0)
+                 <div class="single-sidebar-widget post-category-widget">
+                     <h4 class="single-sidebar-widget__title">Catgory</h4>
+                     <ul class="cat-list mt-20">
+                         @foreach ($categories as $category)
+                             <li>
+                                 <a href="#" class="d-flex justify-content-between">
+                                     <p>{{ $category->name }}</p>
+                                     <p>(03)</p>
+                                 </a>
+                             </li>
+                         @endforeach
+                     </ul>
+                 </div>
+             @endif
 
-                  <div class="single-sidebar-widget post-category-widget">
-                      <h4 class="single-sidebar-widget__title">Catgory</h4>
-                      <ul class="cat-list mt-20">
-                          <li>
-                              <a href="#" class="d-flex justify-content-between">
-                                  <p>Technology</p>
-                                  <p>(03)</p>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#" class="d-flex justify-content-between">
-                                  <p>Software</p>
-                                  <p>(09)</p>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#" class="d-flex justify-content-between">
-                                  <p>Lifestyle</p>
-                                  <p>(12)</p>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#" class="d-flex justify-content-between">
-                                  <p>Shopping</p>
-                                  <p>(02)</p>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#" class="d-flex justify-content-between">
-                                  <p>Food</p>
-                                  <p>(10)</p>
-                              </a>
-                          </li>
-                      </ul>
-                  </div>
 
-                  <div class="single-sidebar-widget popular-post-widget">
-                      <h4 class="single-sidebar-widget__title">Recent Post</h4>
-                      <div class="popular-post-list">
-                          <div class="single-post-list">
-                              <div class="thumb">
-                                  <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb1.png"
-                                      alt="">
-                                  <ul class="thumb-info">
-                                      <li><a href="#">Adam Colinge</a></li>
-                                      <li><a href="#">Dec 15</a></li>
-                                  </ul>
-                              </div>
-                              <div class="details mt-20">
-                                  <a href="blog-single.html">
-                                      <h6>Accused of assaulting flight attendant miktake alaways</h6>
-                                  </a>
-                              </div>
-                          </div>
-                          <div class="single-post-list">
-                              <div class="thumb">
-                                  <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb2.png"
-                                      alt="">
-                                  <ul class="thumb-info">
-                                      <li><a href="#">Adam Colinge</a></li>
-                                      <li><a href="#">Dec 15</a></li>
-                                  </ul>
-                              </div>
-                              <div class="details mt-20">
-                                  <a href="blog-single.html">
-                                      <h6>Tennessee outback steakhouse the
-                                          worker diagnosed</h6>
-                                  </a>
-                              </div>
-                          </div>
-                          <div class="single-post-list">
-                              <div class="thumb">
-                                  <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb3.png"
-                                      alt="">
-                                  <ul class="thumb-info">
-                                      <li><a href="#">Adam Colinge</a></li>
-                                      <li><a href="#">Dec 15</a></li>
-                                  </ul>
-                              </div>
-                              <div class="details mt-20">
-                                  <a href="blog-single.html">
-                                      <h6>Tennessee outback steakhouse the
-                                          worker diagnosed</h6>
-                                  </a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <!-- End Blog Post Siddebar -->
+             <div class="single-sidebar-widget popular-post-widget">
+                 <h4 class="single-sidebar-widget__title">Recent Post</h4>
+                 <div class="popular-post-list">
+                     <div class="single-post-list">
+                         <div class="thumb">
+                             <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb1.png"
+                                 alt="">
+                             <ul class="thumb-info">
+                                 <li><a href="#">Adam Colinge</a></li>
+                                 <li><a href="#">Dec 15</a></li>
+                             </ul>
+                         </div>
+                         <div class="details mt-20">
+                             <a href="blog-single.html">
+                                 <h6>Accused of assaulting flight attendant miktake alaways</h6>
+                             </a>
+                         </div>
+                     </div>
+                     <div class="single-post-list">
+                         <div class="thumb">
+                             <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb2.png"
+                                 alt="">
+                             <ul class="thumb-info">
+                                 <li><a href="#">Adam Colinge</a></li>
+                                 <li><a href="#">Dec 15</a></li>
+                             </ul>
+                         </div>
+                         <div class="details mt-20">
+                             <a href="blog-single.html">
+                                 <h6>Tennessee outback steakhouse the
+                                     worker diagnosed</h6>
+                             </a>
+                         </div>
+                     </div>
+                     <div class="single-post-list">
+                         <div class="thumb">
+                             <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb3.png"
+                                 alt="">
+                             <ul class="thumb-info">
+                                 <li><a href="#">Adam Colinge</a></li>
+                                 <li><a href="#">Dec 15</a></li>
+                             </ul>
+                         </div>
+                         <div class="details mt-20">
+                             <a href="blog-single.html">
+                                 <h6>Tennessee outback steakhouse the
+                                     worker diagnosed</h6>
+                             </a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+     <!-- End Blog Post Siddebar -->
