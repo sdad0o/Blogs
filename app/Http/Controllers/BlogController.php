f173e05f63c8f03c0b73f09c277c79c63737b152
sdad0o <slwdsaw@gmail.com>
2024-12-29 22:57:15 +0300

    finshed all the CRUD for the blogs

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreBlogRequest;
+use App\Http\Requests\UpdateBlogRequest;
 use App\Models\Blog;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class BlogController extends Controller
 {
@@ -68,15 +70,33 @@ class BlogController extends Controller
      */
     public function edit(Blog $blog)
     {
-        //
+        if ($blog->user_id == Auth::user()->id) {
+            $categories = Category::get();
+            return view('theme.blogs.edit', compact('categories', 'blog'));
+        }
+        abort(403);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Blog $blog)
+    public function update(UpdateBlogRequest $request, Blog $blog)
     {
-        //
+
+        if ($blog->user_id == Auth::user()->id) {
+            $data = $request->validated();
+            if ($request->hasFile('image')) {
+                Storage::delete("public/blogs/$blog->image");
+                $image = $request->image;
+                $newImageName = time() . '-' . $image->getClientOriginalName();
+                $image->storeAs('blogs', $newImageName, 'public');
+                $data['image'] = $newImageName;
+            }
+
+            $blog->update($data);
+            return back()->with('blogEditStatus', 'Blog updated successfuly');
+        }
+        abort(403);
     }
 
     /**
@@ -84,7 +104,12 @@ class BlogController extends Controller
      */
     public function destroy(Blog $blog)
     {
-        //
+        if ($blog->user_id == Auth::user()->id) {
+            Storage::delete("public/blogs/$blog->image");
+            $blog->delete();
+            return back()->with('blogDeleteStatus', 'Blog deleted successfuly');
+        }
+        abort(403);
     }
 
     public function myBlogs()
--- resources/views/theme/blogs/my-blogs.blade.php
+++ resources/views/theme/blogs/my-blogs.blade.php
@@ -7,12 +7,17 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
+                    @if (session('blogDeleteStatus'))
+                        <div class="alert alert-success">
+                            {{ session('blogDeleteStatus') }}
+                        </div>
+                    @endif
                     <table class="table">
                         <thead>
                             <tr>
                                 <th scope="col"width="5%">#</th>
                                 <th scope="col">Title</th>
-                                <th scope="col"width="15%">Action</th>
+                                <th scope="col"width="15%">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -24,7 +29,19 @@
                                             <a href="{{ route('blogs.show', ['blog' => $blog]) }}"
                                                 target="_blank">{{ $blog->title }}</a>
                                         </td>
-                                        <td></td>
+                                        <td>
+                                            <a class="btn btn-sm btn-primary mr-2"
+                                                href="{{ route('blogs.show', ['blog' => $blog]) }}">Edit</a>
+                                            <form action="{{ route('blogs.destroy', ['blog' => $blog]) }}" method="post"
+                                                id="delete_form_{{ $blog->id }}" class="d-inline">
+                                                @csrf
+                                                @method('delete')
+                                                <a class="btn btn-sm btn-danger mr-2"
+                                                    href="javascript:$('#delete_form_{{ $blog->id }}').submit();">Delete</a>
+                                            </form>
+
+
+                                        </td>
                                     </tr>
                                 @endforeach
                             @endif
